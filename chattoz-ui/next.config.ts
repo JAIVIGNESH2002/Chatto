e34f1b478ac7ccb4f81285d87c9a5c910112a34b
jaivignesh <jaivignesh.jv05@gmail.com>
2025-10-05 11:13:07 +0530

    App dir config

--- chattoz-ui/next.config.ts
+++ chattoz-ui/next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
+  appDir: "src/app",
 };
 
 export default nextConfig;
